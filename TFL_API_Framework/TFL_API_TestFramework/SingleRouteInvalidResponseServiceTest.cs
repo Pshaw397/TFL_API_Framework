30f74fe069afdaee0d99adf4b5992403ba7222e7
Brandon Campbell <BrandonCampbell98@hotmail.co.uk>
2021-04-30 11:18:48 +0100

    Adds InvalidResponse

---
+++ TFL_API_Framework/TFL_API_TestFramework/SingleRouteInvalidResponseServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API_App.Services;
+using NUnit.Framework;
+
+namespace TFL_API_TestFramework
+{
+    class SingleRouteInvalidResponseServiceTest
+    {
+
+        SingleRouteResponseService _singleRouteResponseService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _singleRouteResponseService = new SingleRouteResponseService();
+            _singleRouteResponseService.MakeRequest("ErrorRequestRoute");
+        }
+
+        [Test]
+        public void Status404()
+        {
+
+            Assert.That(_singleRouteResponseService.numStatusCode, Is.EqualTo(404));
+
+        }
+
+
+    }
+}
